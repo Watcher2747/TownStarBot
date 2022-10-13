c22edd84b77d86d150ced196c05e2cb87906ac3a
Rihards Jansons <49002577+Watcher2747@users.noreply.github.com>
2022-10-13 19:21:22 +0000

    Added stuff

--- connect_to_sheets.py
+++ connect_to_sheets.py
@@ -1,32 +1,43 @@
+import os
 import gspread
 import pandas as pd
 from oauth2client.service_account import ServiceAccountCredentials
 
+my_secret = os.environ['google_json']
+
+
 class Sheets:
-	
-    def __init__(self):
-        pass
-	
-    def save_df_locally(self):
-		# defining the scope of the application
-        scope_app = ['https://spreadsheets.google.com/feeds','https://www.googleapis.com/auth/drive'] 
-		#credentials to the account
-        cred = ServiceAccountCredentials.from_json_keyfile_name('directed-asset-234513-37f8ec096d68.json', scope_app) 
-		# authorize the clientsheet 
-        client = gspread.authorize(cred)
-	
-        sheet = client.open('Town Star Goods Breakdown')
-        sheet_instance = sheet.get_worksheet(0)
-        sheet_records = sheet_instance.get()
 
-        df = pd.DataFrame.from_dict(sheet_records)
-        # view the top records
-        
-        df.columns = df.iloc[0]
-        df = df[1:]
-        
-        df.to_csv('df_original.csv', index=False)
+  def __init__(self):
+    pass
 
-    def get_df_data(self):
-        df = pd.read_csv('df_original.csv')
-        return df
+  def save_df_locally(self):
+    # defining the scope of the application
+    scope_app = [
+      'https://spreadsheets.google.com/feeds',
+      'https://www.googleapis.com/auth/drive'
+    ]
+    #credentials to the account
+    cred = ServiceAccountCredentials.from_json_keyfile_name(
+      my_secret, scope_app)
+    # authorize the clientsheet
+    client = gspread.authorize(cred)
+
+    sheet = client.open('Town Star Goods Breakdown')
+    sheet_instance = sheet.get_worksheet(0)
+    sheet_records = sheet_instance.get()
+
+    df = pd.DataFrame.from_dict(sheet_records)
+    # view the top records
+
+    df.columns = df.iloc[0]
+    df = df[1:]
+
+    df.to_csv('df_original.csv', index=False)
+
+  def get_df_data(self):
+    df = pd.read_csv('df_original.csv')
+    return df
+
+
+print(my_secret)
